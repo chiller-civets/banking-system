795348ea0cab4f9b715755b7ba65ea78b3574697
Elijah Robinson <153487947+chiller-civets@users.noreply.github.com>
2023-12-11 15:11:09 +0700

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,19 @@
+// Customer.java
+
+public class Customer {
+    private String name;
+    private int customerId;
+
+    public Customer(String name, int customerId) {
+        this.name = name;
+        this.customerId = customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+}
